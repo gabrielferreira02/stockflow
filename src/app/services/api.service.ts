aaeb66dc25aa2ca3901fbb7ae6d1547c468654ea
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-04-26 21:11:52 -0300

    feat: Cria service api -  Criado um servoço para chamadas a api com um metodo inicial getCurrency que pega as cotações atuais de ações - Adiciona provideHttpClient nas configurações de provider

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from "../../environments/environment.development"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private url: string = "https://brapi.dev/api";
+  private token: string | undefined;
+
+  private header: {[key: string]: string} | undefined;
+
+  constructor(private http: HttpClient) {
+    this.token = environment.apiToken;
+    console.log(this.token);
+    this.header = {
+      "Authorization": this.token
+    }
+  }
+
+  getCurrency(): Observable<any> {
+    return this.http.get(this.url + "/quote/list", this.header);
+  }
+}
